b3327f8b755c2a9034e1ac377e1239b90c9157b0
AnkushKumar <ankushjha79@gmail.com>
2022-07-19 05:44:45 +0530

    side menu done!

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ if (isset($_SESSION['userLoggedIn'])) {
 					<div class="group">
 						<div class="navItem">
 							<a href="search.php" class="navItemLink">Search
-								<img src="assets/images/icons/search.png" alt="">
+								<img src="assets/images/icons/search.png" class="icon" alt="Search">
 							</a>
 						</div>
 					</div>
